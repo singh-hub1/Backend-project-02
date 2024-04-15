ef9c850dd27019aec1664eb24444c88719ef09b4
vishal singh <vishalkvd2005@gmail.com>
2024-04-15 10:16:01 +0530

    blitz-frontend

--- Supab.js
+++ Supab.js
@@ -43,7 +43,7 @@ app.use(express.json());
 
 // Enable CORS middleware
 app.use(cors({
-  origin: 'https://frontend-project-02.vercel.app/LMS', // Set the origin to allow requests from
+  origin: 'https://frontend-project-02.vercel.app', // Set the origin to allow requests from
   // origin:'http://localhost:3000',
   methods: ['GET', 'POST', 'OPTIONS','DELETE','PUT'], // Set allowed HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Set allowed headers
